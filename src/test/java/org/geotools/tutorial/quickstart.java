a46c2fab49f53568284b4fa8ff2fe0eea7f863b7
jbasile <johannie.basile1@gmail.com>
2016-11-22 15:45:18 -0400

    initial commit (tutorial)

---
+++ src/test/java/org/geotools/tutorial/quickstart.java
@@ -0,0 +1,36 @@
+package org.geotools.tutorial;
+
+import java.io.File;
+
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.data.JFileDataStoreChooser;;
+
+public class quickstart {
+	public static void main(String[] args) throws Exception{
+		File file = JFileDataStoreChooser.showOpenFile("shp",null);
+		if(file == null){
+			return;
+		}
+		
+		FileDataStore store = FileDataStoreFinder.getDataStore(file);
+		SimpleFeatureSource featureSource = store.getFeatureSource();
+		
+		MapContent map = new MapContent();
+		map.setTitle("quickstart");
+		
+		Style style =SLD.createSimpleStyle(featureSource.getSchema());
+		Layer layer =new FeatureLayer(featureSource, style);
+		map.addLayer(layer);
+		
+		JMapFrame.showMap(map);
+	}
+
+}
