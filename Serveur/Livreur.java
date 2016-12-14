cd4a41dc7f275068c636099328a4518316090125
gundamotoko <poubelleend971@gmail.com>
2016-12-14 11:25:26 -0400

    Create Livreur.java
    classe reprśentant les livreur

---
+++ Serveur/Livreur.java
@@ -0,0 +1,12 @@
+package org.geotools.tutorial.quickstart;
+
+public class Livreur {
+	
+	double longitude,latitude;
+	public Thread_Livreur threadlivreur;
+	public Livreur (){
+	//	threadlivreur =new Thread_Livreur();
+		
+	}
+
+}
